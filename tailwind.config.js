699416383f09739e5fb9028b0bb0280c231f7600
Barizahkhasfullah <19ftt1186@student.pb.edu.bn>
2021-03-30 13:24:02 +0800

    added and changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,18 @@ module.exports = {
   purge: [
     './public/**/*.html',
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false, 
   theme: {
-    extend: {},
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
   },
   variants: {
     extend: {},
